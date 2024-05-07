8f0e42f96fc92003284d993c902a8aa0408d2fd0
Mariana-rc01 <rc.mariana.01@gmail.com>
2024-05-07 01:05:54 +0100

    Added big script test

---
+++ bigScriptTest.sh
@@ -0,0 +1,60 @@
+cd bin
+./client execute 5000 -u ./void 10
+./client execute 1400 -p "./hello 7 | grep 1 | wc -l"
+./client execute 5000 -u ./void 13
+./client execute 1050 -p "./hello 5 | grep 1 | wc -l"
+./client execute 5005 -u ./void 30
+./client execute 100 -u ./hello 1
+./client execute 2500 -u ./void 20
+./client execute 1000 -u ./void 13
+./client execute 1200 -p "./hello 5 | grep 1 | wc -l"
+./client execute 199 -u ./hello 1
+./client execute 2555 -u ./void 30
+./client execute 3450 -u ./void 20
+./client execute 1000 -u ./void 10
+./client execute 1000 -p "./hello 15 | grep 1 | wc -l"
+./client execute 1000 -u ./void 13
+./client execute 2500 -u ./void 20
+./client execute 1000 -u ./void 25
+./client execute 1030 -p "./hello 7 | grep 1 | wc -l"
+./client execute 1000 -u ./void 13
+./client execute 1200 -p "./hello 5 | grep 1 | wc -l"
+./client execute 3000 -u ./hello 30
+./client execute 2555 -u ./void 30
+./client execute 3450 -u ./void 20
+./client execute 1000 -u ./void 10
+./client execute 1000 -p "./hello 7 | grep 1 | wc -l"
+./client execute 1000 -u ./void 13
+./client execute 1030 -p "./hello 10 | grep 1 | wc -l"
+./client execute 1030 -p "./hello 5 | grep 1 | wc -l"
+./client execute 1000 -u ./void 10
+./client execute 1030 -p "./hello 7 | grep 1 | wc -l"
+./client execute 1000 -u ./void 13
+./client execute 1200 -p "./hello 5 | grep 1 | wc -l"
+./client execute 100 -u ./hello 1
+./client execute 2555 -u ./void 30
+./client execute 3450 -u ./void 25
+./client execute 1000 -u ./void 10
+./client execute 1000 -p "./hello 25 | grep 1 | wc -l"
+./client execute 1000 -u ./void 13
+./client execute 2500 -u ./void 20
+./client execute 1000 -u ./void 25
+./client execute 1030 -p "./hello 7 | grep 1 | wc -l"
+./client execute 1000 -u ./void 13
+./client execute 1200 -p "./hello 5 | grep 1 | wc -l"
+./client execute 1235 -u ./hello 1
+./client execute 2555 -u ./void 30
+./client execute 3450 -u ./void 20
+./client execute 1000 -u ./void 10
+./client execute 1000 -p "./hello 7 | grep 1 | wc -l"
+./client execute 1000 -u ./void 13
+./client execute 1030 -p "./hello 10 | grep 1 | wc -l"
+./client execute 1030 -p "./hello 10 | grep 1 | wc -l"
+./client execute 1030 -p "./hello 5 | grep 1 | wc -l"
+./client execute 1000 -u ./void 10
+./client execute 1030 -p "./hello 7 | grep 1 | wc -l"
+./client execute 1000 -u ./void 13
+./client execute 1200 -p "./hello 5 | grep 1 | wc -l"
+./client execute 1200 -u ./hello 1
+./client execute 2555 -u ./void 30
+./client execute 1030 -p "./hello 5 | grep 1 | wc -l"
